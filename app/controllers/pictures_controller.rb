a964decfff118070f0cf23ffb59dc4430ec85121
Meera <mjvsharma@outlook.com>
2017-10-10 16:04:08 -0400

    i cant tell if it worked

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,7 +5,19 @@ class PicturesController < ApplicationController
 
   def show
     @picture = Picture.find(params[:id])
-    #store the picture by finding its id in the params hash and selecting for it with its id 
+    #store the picture by finding its id in the params hash and selecting for it with its id
+
+  end
+
+  def new
+    @picture = Picture.new
+    #we are making a new instance of the Picture model
+
+
+  end
+
+  def create
+    render text: "Here's a post request to '/pictures' with the data URL: #{params}"
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   get 'pictures' => 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pictures/new' => 'pictures#new'
+
   get 'pictures/:id' => 'pictures#show'
+
+  post 'pictures' => 'pictures#create'
+  
 end
 
 #match any get request for the localhost3000/pictures to the index method found
