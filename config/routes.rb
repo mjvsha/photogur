fabb1d7610f5e455323529f4ffd06a7f9798022b
Meera <mjvsharma@outlook.com>
2017-10-10 14:43:54 -0400

    made a model for pictures and a table

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,6 @@ Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#match any get request for the localhost3000/pictures to the index method found
+#in the pictures controller
