a6a2316d0cde7bd3e1d4f1b3cbe17d2f24f5ffdd
Meera <mjvsharma@outlook.com>
2017-10-10 15:14:22 -0400

    made separate pages for each picture by specifying an instance variable equal to the id attribute of the Pictures database. displayed title, artist, picture on individual pages

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,13 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = Picture.all 
+    @pictures = Picture.all
   end
+
+  def show
+    @picture = Picture.find(params[:id])
+    #store the picture by finding its id in the params hash and selecting for it with its id 
+
+  end
+
 
 end
