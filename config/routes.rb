1135f67ee3d1cbbb488f4d7e45e742983489cff1
Meera <mjvsharma@outlook.com>
2017-10-10 16:28:38 -0400

    added nav links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'pictures#index'
 
   get 'pictures' => 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   get 'pictures/:id' => 'pictures#show'
 
   post 'pictures' => 'pictures#create'
-  
+
 end
 
 #match any get request for the localhost3000/pictures to the index method found
