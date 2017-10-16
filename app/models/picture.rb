d47522fe211a9ee825a61e9b9b8ca2da3dce23b9
Meera <mjvsharma@outlook.com>
2017-10-16 16:32:14 -0400

    added validations to the inputs

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,25 @@
 class Picture < ApplicationRecord
+  #:artist, :title, :url
+  validates :artist, :url, presence: { message: "unfortunately, this cant be blank "} 
+  #title must be min 3 char, max 20 char
+  validates :title, length: {in: 3..20}
+  #url must be unique from other urls
+  validates :url, uniqueness: true
+
+  #this is the model and its looking at the database and it is looking for artist, title, url in the database
+  #it is not looking at the form per se, but at the database columns for artist, title, url
+
+
   def self.more_than_month
     Picture.where("created_at < ?", "2017-09-14")
   end
+
+  def self.picture_created_in_year
+     Picture.where("created_at >= ?", "#{year}-01-01 01:01:01").where("created_at <= ?", "#{year}-12-31 23:59:59")
+
+  end
+
+
 
 
 
