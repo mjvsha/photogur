f88160ef9f32b0fa6af9015c5f8f3fde474adcde
Meera <mjvsharma@outlook.com>
2017-10-20 13:48:00 -0400

    wrote code for navigation and used helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
 
-def current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  #we create a helper method here to show the log in status of the user
+  #this will talk to the associated view, and display a message according to the status of the current_user variable
   
+
+  helper_method :current_user
 end
