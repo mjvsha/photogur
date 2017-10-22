b48956f5c1a08407857436b9e4eabe71adf25b07
Meera <mjvsharma@outlook.com>
2017-10-21 20:33:03 -0400

    edit, destroy create methods still working with before_method additon

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,9 @@
 class PicturesController < ApplicationController
 
   before_action :ensure_logged_in, except: [:show, :index]
+  before_action :load_picture, only: [:show, :edit, :update, :destroy]
+  # before_action :ensure_user_owns_picture
+  #lets compare the crrent_user's id to the suer_id of the picture that the user id trying to edit, update, delete
 
   def index
     @pictures = Picture.all
@@ -16,9 +19,14 @@ class PicturesController < ApplicationController
   def new
     @picture = Picture.new
     #we are making a new instance of the Picture model
+  end
 
+  def load_picture
+    @picture = Picture.find(params[:id])
+    #with this line of code, we can remove the exact line from the edit, update and destroy actions elsewhere in our code
 
   end
+
 
   def create
     @picture = Picture.new
@@ -37,10 +45,10 @@ class PicturesController < ApplicationController
   end
 
   def edit
-    @picture = Picture.find(params[:id])
+    # @picture = Picture.find(params[:id])
   end
   def update
-    @picture = Picture.find(params[:id])
+    # @picture = Picture.find(params[:id])
 
     @picture.title  = params[:picture][:title]
     @picture.artist = params[:picture][:artist]
@@ -54,10 +62,13 @@ class PicturesController < ApplicationController
   end
 
   def destroy
-    @picture = Picture.find(params[:id])
+    # @picture = Picture.find(params[:id])
     @picture.destroy
     redirect_to '/pictures'
 
   end
+
+
+
 
 end
